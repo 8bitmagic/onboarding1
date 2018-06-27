38e3f9e3a8946a25189ee74b1516f5e91140bebc
8bitmagic <will.chan.33@gmail.com>
2018-06-27 17:24:31 -0400

    test

---
+++ Desktop/PSRewrite/PSrewrite/PSRewrite/BCMSD/web/iceconfig.js
@@ -0,0 +1,48 @@
+﻿//Main Settings
+var iceSwitchID = '11001';
+var imrFQDN = "imr.domain.com";
+var imrWebApiPort = "8501";
+var imrHttpsPort = "8301";
+var iceAdminUseCitrix = false;
+var iceAdminCitrixUrl = "";
+
+
+// target duration in minutes
+var TargetDuration = 2;
+// duration/TargetDuration < WarningLevel => green
+// duration/TargetDuration < AlertLevel => orange
+// duration/TargetDuration > AlertLevel => red
+var WarningLevel = 70;
+var AlertLevel = 90;
+
+//This is where customs can add any extra parameters for iceMonitor here!
+//Note: Logs, and iceIMRService address should not be added here, iceMonitor gets these values from signalR instead.
+var iceMonitorExtraParams = 'IdleTimeCheck=10,TileWidth=300,TileFontColor=#FF000000,TileBarHeight=20,BarTextAlign=Center,BarTextVisible=1,LED=0';
+
+//This configures how logging to the server should act
+//Silverlight will be passed these values on load
+var logToServer = true;          //true - to send log message to iceLogger on IIS, false to disable it
+var logToServerLevelJS = 1;      //Levels: LOG_ERROR = 1, LOG_INFO = 2, LOG_VERBOSE = 3, LOG_DEBUG = 4
+var logToServerLevelMonitor = 2; //Levels: FATAL = 1, ERROR = 2, WARNING = 3, INFO = 4, VERBOSE = 5, DEBUG = 6
+var logToServerURL = "LogCollector.aspx/LogMessage"; //URL of a restful method to send the logs to
+var logToServerUsers = null;     //set null to log all users with the above level or add specific user ids in the list to only log specific users.
+//var logToServerUsers = ["9999","10001"]; //example of 2 users that send logs to iceLogger
+
+//This variable is to set iceMonitor to reload if we lost connection to either signalR to IIS or IIS to iceIMRService, or iceIMRService to ice server
+//each one can result in missed events rendering the data not valid.
+//we will unload monitor and restart it! (Only if this is set to true)
+var restartMonitorConnectionFaults = true;
+
+//Timeout value in seconds.
+var webAPITimeout = 120;
+
+//Server Locations
+var iceIdentityServer = "https://" + imrFQDN + ":" + imrWebApiPort + "/identity";
+var iceWebApiServer = "https://" + imrFQDN + ":" + imrWebApiPort + "/webapi";
+var iceSignalRServer = "https://" + imrFQDN + ":" + imrWebApiPort + "/signalr/signalr";
+var iceIMRServiceServer = "https://" + imrFQDN + ":" + imrHttpsPort + "/iceIMRService";
+
+//We want to force websocket only for signalR transport.
+var signalRWebSocketOnly = true;
+var enableADFSLogin = false; //Enable this if customer want ADFS login
+
---
+++ Desktop/PSRewrite/PSrewrite/PSRewrite/rewrite.ps1
@@ -0,0 +1,99 @@
+﻿
+<#--adding init values--#>
+function Set-OrAddIniValue
+{
+    Param(
+        [string]$FilePath,
+        [hashtable]$keyValueList
+    )
+
+    $content = Get-Content $FilePath
+
+    $keyValueList.GetEnumerator() | ForEach-Object {
+        if ($content -match "^$($_.Key)=")
+        {
+            $content= $content -replace "^$($_.Key)=(.*)", "$($_.Key)=$($_.Value)"
+        }
+        else
+        {
+            $content += "$($_.Key)=$($_.Value)"
+        }
+    }
+
+    $content | Set-Content $FilePath
+}
+
+<#--makes customer filedirecotry--#>
+function createDirectory($cName){
+    New-Variable -Name dir1 -Value "C:\Users\WCHAN\Desktop\PSRewrite\$cName" -Scope Global
+    New-Item -Path $dir1 -ItemType Directory -Force
+    cp -Force -Recurse C:\Users\WCHAN\Desktop\PSRewrite\clean\* $dir1
+    
+    
+}
+
+
+<#---cleanfiles #>
+$list = @()
+$list += "\web\iceconfig.js"
+$list +=  "\web\Web.config"
+$list += "\packager\install.ini"
+$list += "\recorder\iceRecordingManager.exe.config"
+$list += "\recorder\iceUCMARecorder.exe.config"
+$list += "\imr\iceIMRHostService.exe.config"
+
+
+<#---vars---#>
+$cName = "BCMSD"
+$webServer = "web12.icescape.com"
+$switchID = "11001"
+$sqlServer = "ctdvmsql01.ctd.local"
+$switchDB = "ctdvmice01Switch"+$switchID
+$sqlUser = "sa"
+$sqlPwd = "ctt^sql^288"
+
+<#-- readhost--#>
+<#
+Set-OrAddIniValue -FilePath "C:\Users\WCHAN\Desktop\PSRewrite\install.ini"  -keyValueList @{
+    Server = "testserver.icescape.com"
+    Port = "20702"
+    SwitchID = "$switchID"
+    GlobalConfigLocation = "https://"+$webServer+"/"+$cName+"/iceBARConfig/iceBARWebService.asmx"
+
+}
+
+Set-OrAddIniValue -FilePath "C:\Users\WCHAN\Desktop\PSRewrite\install.ini"  -keyValueList @{
+    Server = "testserver.icescape.com"
+    Port = "20702"
+    SwitchID = "$switchID"
+    GlobalConfigLocation = "https://"+$webServer+"/"+$cName+"/iceBARConfig/iceBARWebService.asmx"
+
+}
+
+# Load the file
+$FileContentVariable = Get-Content -Path .\textfile.txt
+
+# Replace the value for "Setting1" with "This is my new setting" - note the use of tick marks (`) for escaping quotes
+$FileContentVariable = $FileContentVariable -replace ("Setting1=`"This is a placeholder`"", "Setting1=`"This is my new setting`"")
+
+# Replace the value for "Setting2" with "Here is an iguana" but use a regular expression for the search this time.
+$FileContentVariable = $FileContentVariable -replace ("Setting2=.+", "Setting2=`"Here is an iguana`"")
+
+# Export the new variable to a new text file (or to the old file if you're updating a config file)
+$FileContentVariable | Out-File -FilePath .\textfile_updated.txt
+
+
+
+
+#>
+
+$fList = @()
+<#--actions--#>
+createDirectory($cName) --quiet
+
+<#
+for($i = 0; $i -lt $list.Count; $i++)
+{
+   $fList += $dir1 + $list[$i]
+   foreach
+}#>
